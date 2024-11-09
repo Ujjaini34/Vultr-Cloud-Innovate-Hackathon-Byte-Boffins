67919e4643b0858e8e8e3ad4fe3bbc6ca854c238
Ujjaini Halder <131608638+Ujjaini34@users.noreply.github.com>
2024-11-09 06:11:03 +0530

    Delete start.py

--- start.py
+++
@@ -1,5 +0,0 @@
-import os
-from app import app  # Replace 'app' with the name of your main file (without .py)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3000)))
